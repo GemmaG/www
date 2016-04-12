6199c2fd60f114e015a5d57a2032a4b70477f34c
Gemma Gordon <gemma.t.gordon@gmail.com>
2016-04-12 13:53:28 +0100

    Removed old files

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh -e
-cd bin && make && cd ..
-cd updates && ../bin/_build/lib/www.native ../pages && cd ..
-cd pages && env PATH=../ucampas:$PATH ucampas -i -r1 index people projects activity
